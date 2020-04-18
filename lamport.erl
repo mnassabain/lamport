9f44a12f0eeff4ca029f6913df89226b5c649829
Marco Nassabain <marco.nassabain@hotmail.com>
2020-04-18 15:07:18 +0200

    spawning n process

--- lamport.erl
+++ lamport.erl
@@ -3,7 +3,7 @@
 
 -module(lamport).
 
--export([test/1, make_list/1]).
+-export([test/1, process/2]).
 
 
 make_list(0) ->
@@ -23,8 +23,13 @@ spawnN(N) when N > 0 ->
 spawnN(0, _) ->
     done;
 spawnN(I, N) ->
-    io:format("~w~n", [list_to_atom("pid" ++ integer_to_list(N))]),
+    % io:format("~w~n", [list_to_atom("pid" ++ integer_to_list(N))]),
+    spawn(lamport, process, [I, make_list(N)]),
     spawnN(I - 1, N).
+
+
+process(I, Vector) ->
+    io:format("I am pid ~w, my vector is ~w~n", [I, Vector]).
 
 
 test(N) ->
