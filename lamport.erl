fa22780fbb68be3deba1e3d2d9c8839a3e69a741
Marco Nassabain <marco.nassabain@hotmail.com>
2020-04-18 14:52:29 +0200

    implemented lamport vector creation

--- lamport.erl
+++ lamport.erl
@@ -3,12 +3,23 @@
 
 -module(lamport).
 
--export([test/1]).
+-export([test/1, make_list/1]).
+
+
+make_list(0) ->
+    [];
+make_list(1) ->
+    [0];
+make_list(N) ->
+    [0 | make_list(N - 1)].
 
 
 spawnN(0) ->
     done;
 spawnN(N) when N > 0 ->
+    % register & spawn process
+    % pass N to process so that it knows its index in table
+    % pass vector (list) of n-elements
     io:format("~w~n", [list_to_atom("pid" ++ integer_to_list(N))]),
     spawnN(N - 1).
 
